8145936854ac2fab76864a20dda790777859918a
Jamsheed Ali Ullatt <corra-160@Corra-160.local>
2024-07-26 16:30:04 +0530

    Added youtube embed files

---
+++ blocks/youtube-embed/youtube-embed.js
@@ -0,0 +1,22 @@
+export default function youtubevideo(block) {
+  const urlElement = block.querySelector('div');
+  if (!urlElement) return;
+
+  const url = urlElement.textContent.trim();
+
+  const videoIdmatch = url.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/);
+  if (!videoIdmatch || !videoIdmatch[1]) return;
+
+  const videoId = videoIdmatch[1];
+
+  const iframe = document.createElement('iframe');
+  iframe.src = `https://www.youtube.com/embed/${videoId}`;
+  iframe.frameborder = '0';
+  iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+  iframe.width = '100%';
+  iframe.height = '500px';
+  iframe.allowFullscreen = true;
+
+  block.innerHTML = '';
+  block.appendChild(iframe);
+}
